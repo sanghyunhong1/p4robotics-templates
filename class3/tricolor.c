3a7ef9da5cda1759534f73628765766d345c463b
shong7 <shong7@ford.com>
2021-07-23 08:47:00 -0400

    class 3: tricolor.c changed

--- class3/tricolor.c
+++ class3/tricolor.c
@@ -7,9 +7,10 @@ int main(void) {
     // at the very beginning of your program, making the table
     for (int i = 0; i < N_TRITS; i++) {
         trit_encodings[i] = calloc(sizeof(char), 6);
+        // ADD YOUR CODES HERE
     }
 
-
+    // ADD YOUR CODES HERE
 
     // at the very end of your program, cleaning up memory
     for (int i = 0; i < N_TRITS; i++) {
