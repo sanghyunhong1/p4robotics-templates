fb98d5fe44c382adb91b0acf01da667798c161dc
shong7 <shong7@ford.com>
2021-07-26 13:58:54 -0400

    class5: golomb.c added

---
+++ class5/golomb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// ADD YOUR CODES HERE
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <max golomb number>\n", argv[0]);
+        return 1;
+    }
+
+    // ADD YOUR CODES HERE
+
+    return 0;
+}
